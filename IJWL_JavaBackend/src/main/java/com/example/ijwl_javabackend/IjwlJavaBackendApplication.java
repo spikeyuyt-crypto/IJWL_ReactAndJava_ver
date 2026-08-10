75f2f8b89473b7fe8ad7aaa3572146b326578209
yt <spike.yt.yt@gmail.com>
2026-08-10 17:48:00 +0900

    init

---
+++ IJWL_JavaBackend/src/main/java/com/example/ijwl_javabackend/IjwlJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ijwl_javabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IjwlJavaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IjwlJavaBackendApplication.class, args);
+    }
+
+}
---
+++ IJWL_JavaBackend/src/test/java/com/example/ijwl_javabackend/IjwlJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ijwl_javabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IjwlJavaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
